11b7b02eaf1ae2d9a2fead6fc7577e7e6f77f340
PrettyMing <pro.mingian@gmail.com>
2024-07-26 00:03:27 -0400

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi ='http://localhost:7071/api/GetResumeCounter'
+const functionApiUrl = 'https://getmingcounter.azurewebsites.net/api/GetResumeCounter?code=wHlWN0FpKRt_CfcR5I3UA4352es5cax1gIOLPOiFQycfAzFuZ4z6Sg%3D%3D'
+const localFunctionApi ='http://localhost:7071/api/GetResumeCounter'
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
